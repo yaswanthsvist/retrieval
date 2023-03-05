a0834896b5257cc6863c96537414e42321471788
yaswanthsvist <yaswanthsvist@gmail.com>
2023-03-05 22:41:50 +0530

    support id reference attribute

--- demo/demo1/scenarios.js
+++ demo/demo1/scenarios.js
@@ -1,7 +1,7 @@
 /*
  * Demonstrates how to use the Retrieval module via 4 quick examples.
- * Namely, 
- * (1) search for 'theme and variations'; note that docs where both terms 
+ * Namely,
+ * (1) search for 'theme and variations'; note that docs where both terms
  * appear are ranked higher
  * (2) in 'opera browser', the word 'browser' doesn't belong to any document,
  * so the search engine only returns documents with the word 'opera'
@@ -11,48 +11,54 @@
  * also note that duplicate documents were found, which is fine because they
  * exist in the document collection.
  */
-const path = require("path");
+const path = require('path');
 
 // Import the search engine.
-const Retrieval = require(path.join(__dirname, "..", "..", "src", "Retrieval.js"));
-const docs = require("./data/music-collection"); // Load the texts to search.
+const Retrieval = require(path.join(
+  __dirname,
+  '..',
+  '..',
+  'src',
+  'Retrieval.js'
+));
+const docs = require('./data/music-collection'); // Load the texts to search.
 
 // 1st step: construct an object, feeding two parameters for bm25.
-let rt = new Retrieval(K=2, B=0.75);
+let rt = new Retrieval((K = 2), (B = 0.75));
 
 // 2nd step: index the texts loaded above.
-rt.index(docs);
+rt.index(docs.map((value, index) => ({ id: 'id_' + index, value })));
 
 // 3rd step: search.
-console.log("------------------------------------------------------------");
+console.log('------------------------------------------------------------');
 console.log("Top 5 search results for the query 'theme and variations':\n");
-let results = rt.search("theme and variations", 5);
-results.map(result => console.log(result));
+let results = rt.search('theme and variations', 5);
+results.map((result) => console.log(result));
 // 04 - Theme & Variations In G Minor.flac
 // 17 - Rhapsody On A Theme of Paganini - Variation 18.flac
 // 01 - Diabelli Variations - Theme Vivace & Variation 1 Alla Marcia Maestoso.flac
 // 07 - Rhapsody On A Theme of Paganini (Introduction and 24 Variations).flac
 // 10 - Diabelli Variations - Variation 10 Presto.flac
 
-console.log("\n\n----------------------------------------------------------");
-console.log("Top 5 search results for the query 'opera browser':\n")
+console.log('\n\n----------------------------------------------------------');
+console.log("Top 5 search results for the query 'opera browser':\n");
 results = rt.search('opera browser', 5);
-results.map(result => console.log(result));
+results.map((result) => console.log(result));
 // 01 - Così Fan Tutte, Opera, K. 588- Overture.flac
 // 09 - Il Trovatore, Opera- Act 4. Scene 1. Miserere.flac
 // 04 - Der Rosenkavalier, Opera, Op. 59 (TrV 227)- Unidentified Excerpt.flac
 // 07 - Così Fan Tutte, Opera, K. 588- Act 1. Scene 3. In Uomini, In Soldati.flac
 // 11 - Così Fan Tutte, Opera, K. 588- Act 1. Scene 3. E Voi Ridete.flac
 
-console.log("\n\n----------------------------------------------------------");
-console.log("Top 5 search results for the query 'does not exist':\n")
+console.log('\n\n----------------------------------------------------------');
+console.log("Top 5 search results for the query 'does not exist':\n");
 results = rt.search('does not exist', 5);
-results.map(result => console.log(result));
+results.map((result) => console.log(result));
 
-console.log("\n\n----------------------------------------------------------");
-console.log("Top 5 search results for the query 'blues':\n")
+console.log('\n\n----------------------------------------------------------');
+console.log("Top 5 search results for the query 'blues':\n");
 results = rt.search('blues', 5);
-results.map(result => console.log(result));
+results.map((result) => console.log(result));
 // 09 - Weary Blues.flac
 // 09 - Weary Blues.flac
 // 11 - The Blue Danube Op. 314.flac
--- src/Retrieval.js
+++ src/Retrieval.js
@@ -1,7 +1,7 @@
 /*
  * Specifies a class whose primary functionality is taking an array of texts,
- * parsing it using natural language processing, indexes it via Okapi-BM25 
- * representation, and enabling full-text search on this collection of 
+ * parsing it using natural language processing, indexes it via Okapi-BM25
+ * representation, and enabling full-text search on this collection of
  * documents.
  */
 const _ = require('lodash');
@@ -11,67 +11,61 @@ const query2vec = require('./query/query2vec.js');
 const chunkFilterStem = require('./nlp/chunk_filter_stem.js');
 const topIndices = require('./score_selection/top_indices.js');
 
-module.exports = (function() {
-	//CONSTRUCTOR
-  	var Retrieval = function(K=1.6, B=0.75) {
-		this.docArray = [];
-		this.K = K;
-		this.B = B;
-		this.docIndex = {};
-		this.termIndex = {};
-  	};
+module.exports = (function () {
+  //CONSTRUCTOR
+  var Retrieval = function (K = 1.6, B = 0.75) {
+    this.docArray = [];
+    this.K = K;
+    this.B = B;
+    this.docIndex = {};
+    this.termIndex = {};
+    this.docIndexToId = [];
+  };
 
-     
-  	//METHODS
-  	Retrieval.prototype.index = function(docArray) {
-		this.docArray = docArray;
+  //METHODS
+  Retrieval.prototype.index = function (docArray) {
+    this.docArray = docArray.map((doc) => doc.value);
+    this.docIndexToId = docArray.map((doc) => doc.id);
 
-		// Convert the document list to a sparse matrix search index.
-		let bm25 = new Bm25(corpusMatr = this.docArray.map(chunkFilterStem),
-								  K = this.K,
-                          B = this.B
-                         );
+    // Convert the document list to a sparse matrix search index.
+    let bm25 = new Bm25(
+      (corpusMatr = this.docArray.map(chunkFilterStem)),
+      (K = this.K),
+      (B = this.B)
+    );
 
-		// Create a sparse matrix with initial data and the 'number' datatype.
-		this.docIndex = math.sparse(bm25.buildMatr(), 
-											 'number'
-											);
+    // Create a sparse matrix with initial data and the 'number' datatype.
+    this.docIndex = math.sparse(bm25.buildMatr(), 'number');
 
-		// Create a reverse index for fast keyword search.
-		this.termIndex = bm25.getTerms();
-	};
+    // Create a reverse index for fast keyword search.
+    this.termIndex = bm25.getTerms();
+  };
 
+  Retrieval.prototype.search = function (query_, N = 10) {
+    // Project query to vector space spanned by the indexed documents.
 
-  	Retrieval.prototype.search = function(query_, N=10) {
-		// Project query to vector space spanned by the indexed documents.
-      		
-      // STEP 1: Vectorize the query string and then multiply it by the bm25 matrix, which equals
-      // a vector of bm25 scores for this query. Finally, concat & valueOf turns mathjs vector object 
-      // into an array of document scores.
-      try {
-			var queryVector = query2vec(query_, 
-												 this.termIndex
-												);
-		}
-		catch(error) {
-			return [];
-		}
-      
-      let docScores = [].concat(...math.multiply(this.docIndex,
-																 queryVector
-																)
-													.valueOf()
-										 );
+    // STEP 1: Vectorize the query string and then multiply it by the bm25 matrix, which equals
+    // a vector of bm25 scores for this query. Finally, concat & valueOf turns mathjs vector object
+    // into an array of document scores.
+    try {
+      var queryVector = query2vec(query_, this.termIndex);
+    } catch (error) {
+      return [];
+    }
 
+    let docScores = [].concat(
+      ...math.multiply(this.docIndex, queryVector).valueOf()
+    );
 
-		// STEP 2: Retrieve the 10 highest scoring document indices.
-		let topNIndices = topIndices(docScores).slice(0, N);
+    // STEP 2: Retrieve the 10 highest scoring document indices.
+    let topNIndices = topIndices(docScores).slice(0, N);
 
+    // STEP 3: Retrieve the documents best matching the query.
+    return topNIndices.map((idx) => ({
+      value: this.docArray[idx],
+      id: this.docIndexToId[idx]
+    }));
+  };
 
-		// STEP 3: Retrieve the documents best matching the query.
-		return topNIndices.map((idx) => this.docArray[idx])
-	};
-
-
-   return Retrieval;
+  return Retrieval;
 })();
